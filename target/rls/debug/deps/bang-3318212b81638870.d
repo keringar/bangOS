e781db355ee9ca3746c7ab23b54d055a9ab222ff
keringar <wanrunhu@gmail.com>
2017-12-21 23:53:58 -0800

    Remove target dir

--- target/rls/debug/deps/bang-3318212b81638870.d
+++
@@ -1,6 +0,0 @@
-/home/keringar/projects/bang/target/rls/debug/deps/bang-3318212b81638870.rmeta: src/lib.rs src/vga_buffer.rs
-
-/home/keringar/projects/bang/target/rls/debug/deps/bang-3318212b81638870.d: src/lib.rs src/vga_buffer.rs
-
-src/lib.rs:
-src/vga_buffer.rs:
--- target/rls/debug/deps/bang-a3f5a26f777ac5a7.d
+++
@@ -1,6 +0,0 @@
-/home/keringar/projects/bang/target/rls/debug/deps/bang-a3f5a26f777ac5a7.rmeta: src/lib.rs src/vga_buffer.rs
-
-/home/keringar/projects/bang/target/rls/debug/deps/bang-a3f5a26f777ac5a7.d: src/lib.rs src/vga_buffer.rs
-
-src/lib.rs:
-src/vga_buffer.rs:
